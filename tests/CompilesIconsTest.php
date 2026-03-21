2830b6b0344378cd07905e0f234f91464a622298
Swapnil Sarwe <swapnilsarwe@gmail.com>
2026-03-21 18:47:56 +0530

    laravel 13 compatibility

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -12,7 +12,7 @@ use Codeat3\BladePhosphorIcons\BladePhosphorIconsServiceProvider;
 class CompilesIconsTest extends TestCase
 {
     /** @test */
-    public function it_compiles_a_single_anonymous_component()
+    public function test_it_compiles_a_single_anonymous_component()
     {
         $result = svg('phosphor-alarm')->toHtml();
 
@@ -25,7 +25,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_add_classes_to_icons()
+    public function test_it_can_add_classes_to_icons()
     {
         $result = svg('phosphor-alarm', 'w-6 h-6 text-gray-500')->toHtml();
 
@@ -37,7 +37,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_add_styles_to_icons()
+    public function test_it_can_add_styles_to_icons()
     {
         $result = svg('phosphor-alarm', ['style' => 'color: #555'])->toHtml();
 
@@ -49,7 +49,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_add_default_class_from_config()
+    public function test_it_can_add_default_class_from_config()
     {
         Config::set('blade-phosphor-icons.class', 'awesome');
 
@@ -64,7 +64,7 @@ class CompilesIconsTest extends TestCase
     }
 
     /** @test */
-    public function it_can_merge_default_class_from_config()
+    public function test_it_can_merge_default_class_from_config()
     {
         Config::set('blade-phosphor-icons.class', 'awesome');
 
